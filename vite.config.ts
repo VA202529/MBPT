1ddedace7814d0988ea09037a806e059007a5538
WWeu09 <you@example.com>
2026-06-20 12:27:00 +0200

    fix vite config for cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
@@ -6,6 +7,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [
+    cloudflare({ viteEnvironment: { name: "ssr" } }),
     tsconfigPaths(),
     tanstackStart({
       server: { entry: "server" },
